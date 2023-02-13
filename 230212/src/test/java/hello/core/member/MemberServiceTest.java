b3953a1e0d362e151a9190a987ff96e694196473
seungjjun <il1uso@naver.com>
2023-02-13 23:53:34 +0900

    Spring IoC, DI, 컨테이너 예제를 이용해 공부

---
+++ 230212/src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,27 @@
+package hello.core.member;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+public class MemberServiceTest {
+    MemberService memberService;
+
+    @BeforeEach
+    void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
+
+    @Test
+    void join() {
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        assertThat(member).isEqualTo(findMember);
+    }
+}
