48486ab574ac7231ea62b78906b298efb0fb23df
seungjjun <il1uso@naver.com>
2023-08-24 23:21:47 +0900

    security SessionCreationPolicy, SessionFixation 설정
    - session이 필요할때만 session이 생성되도록 설정
    - 세션 고정 공격 보호 changeId() 메서드를 이용해 새로 인증될때 마다 새로 session 생성하는 전략

--- 230817/src/main/java/com/example/demo/config/SecurityConfig.java
+++ 230817/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -2,9 +2,13 @@ package com.example.demo.config;
 
 import com.example.demo.security.SimpleAuthenticationFilter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -12,15 +16,15 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-            .antMatchers("/user")
-            .authenticated()
+        http.requestMatchers(request ->
+                request.antMatchers(HttpMethod.GET, "/user"))
+            .addFilterAt(new SimpleAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+            .sessionFixation().changeSessionId()
             .and()
-            .authorizeRequests()
-            .anyRequest()
-            .permitAll()
+                .cors(Customizer.withDefaults()).csrf(AbstractHttpConfigurer::disable)
+                .authorizeRequests().anyRequest().permitAll()
             .and()
-            .addFilterAt(new SimpleAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+                .addFilterAt(new SimpleAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
