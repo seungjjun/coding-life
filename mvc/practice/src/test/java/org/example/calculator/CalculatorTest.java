c989966c8b68e58b8067f17f283bfe25b76679a9
seungjjun <il1uso@naver.com>
2023-09-17 14:34:20 +0900

    practice oop with calculator

---
+++ mvc/practice/src/test/java/org/example/calculator/CalculatorTest.java
@@ -0,0 +1,32 @@
+package org.example.calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class CalculatorTest {
+    @DisplayName("사칙연산을 수행한다.")
+    @ParameterizedTest
+    @MethodSource("formulaAndResult")
+    void additionTest(int operand1, String operator, int operand2, int result) {
+        int calculateResult = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
+
+        assertThat(calculateResult).isEqualTo(result);
+    }
+
+    private static Stream<Arguments> formulaAndResult() {
+        return Stream.of(
+                arguments(1, "+", 2, 3),
+                arguments(1, "-", 2, -1),
+                arguments(2, "*", 3, 6),
+                arguments(4, "/", 2, 2)
+        );
+    }
+}
