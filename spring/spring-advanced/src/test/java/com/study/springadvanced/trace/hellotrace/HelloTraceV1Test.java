695003832e15f5be1ff12a3c39f8a9f99de4ab42
seungjjun <il1uso@naver.com>
2024-01-14 19:59:37 +0900

    add: log trace example code

---
+++ spring/spring-advanced/src/test/java/com/study/springadvanced/trace/hellotrace/HelloTraceV1Test.java
@@ -0,0 +1,23 @@
+package com.study.springadvanced.trace.hellotrace;
+
+import com.study.springadvanced.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloTraceV1Test {
+
+    @Test
+    void begin_end() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.end(status);
+    }
+
+    @Test
+    void begin_exception() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.exception(status, new IllegalStateException());
+    }
+}
