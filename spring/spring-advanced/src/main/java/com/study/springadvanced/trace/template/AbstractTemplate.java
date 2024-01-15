af34ab595cccb4c70c9b08affae80808bcb7455e
seungjjun <il1uso@naver.com>
2024-01-15 22:26:37 +0900

    add: template method pattern

---
+++ spring/spring-advanced/src/main/java/com/study/springadvanced/trace/template/AbstractTemplate.java
@@ -0,0 +1,29 @@
+package com.study.springadvanced.trace.template;
+
+import com.study.springadvanced.trace.TraceStatus;
+import com.study.springadvanced.trace.logtrace.LogTrace;
+
+public abstract class AbstractTemplate<T> {
+    private final LogTrace trace;
+
+    public AbstractTemplate(LogTrace trace) {
+        this.trace = trace;
+    }
+
+    public T execute(String message) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin(message);
+
+            T result = call();
+
+            trace.end(status);
+            return result;
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+
+    protected abstract T call();
+}
