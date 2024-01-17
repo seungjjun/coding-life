44ae59d983952d2a7760023f74047282175c334a
seungjjun <il1uso@naver.com>
2024-01-17 20:05:54 +0900

    add: proxy pattern

--- spring/spring-advanced/src/main/java/com/study/springadvanced/SpringAdvancedApplication.java
+++ spring/spring-advanced/src/main/java/com/study/springadvanced/SpringAdvancedApplication.java
@@ -1,9 +1,13 @@
 package com.study.springadvanced;
 
+import com.study.springadvanced.proxy.config.AppV1Config;
+import com.study.springadvanced.proxy.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@Import({AppV1Config.class, AppV2Config.class})
+@SpringBootApplication(scanBasePackages = "com.study.springadvanced.proxy.app")
 public class SpringAdvancedApplication {
 
     public static void main(String[] args) {
---
+++ spring/spring-advanced/src/test/java/com/study/springadvanced/proxy/pureproxy/proxy/code/Subject.java
@@ -0,0 +1,5 @@
+package com.study.springadvanced.proxy.pureproxy.proxy.code;
+
+public interface Subject {
+    String operation();
+}
