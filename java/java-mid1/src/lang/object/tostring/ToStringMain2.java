4e9a93d3da9525023960cb5628304b0de7a564d1
seungjjun <il1uso@naver.com>
2024-08-17 23:45:48 +0900

    add java object study code

---
+++ java/java-mid1/src/lang/object/tostring/ToStringMain2.java
@@ -0,0 +1,21 @@
+package lang.object.tostring;
+
+public class ToStringMain2 {
+
+	public static void main(String[] args) {
+		Car car = new Car("Model Y");
+		Dog dog1 = new Dog("dog1", 2);
+		Dog dog2 = new Dog("dog2", 5);
+
+		System.out.println(car.toString());
+		System.out.println(dog1.toString());
+		System.out.println(dog2.toString());
+
+		ObjectPrinter.print(car);
+		ObjectPrinter.print(dog1);
+		ObjectPrinter.print(dog2);
+
+		String refValue = Integer.toHexString(System.identityHashCode(dog1));
+		System.out.println("refValue = " + refValue);
+	}
+}
