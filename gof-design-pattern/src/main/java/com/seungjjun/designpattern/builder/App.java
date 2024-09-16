25603ca44b88d15c375dd31f679729d402374f4a
seungjjun <il1uso@naver.com>
2024-09-16 14:16:16 +0900

    builder pattern

--- gof-design-pattern/src/main/java/com/seungjjun/designpattern/builder/App.java
+++ gof-design-pattern/src/main/java/com/seungjjun/designpattern/builder/App.java
@@ -1,7 +1,25 @@
 package com.seungjjun.designpattern.builder;
 
+import java.time.LocalDate;
+
 public class App {
 
 	public static void main(String[] args) {
+		TourPlanBuilder builder = new DefaultTourBuilder();
+		TourPlan plan = builder.title("여행")
+			.nightsAndDays(2, 3)
+			.startDate(LocalDate.of(2020, 12, 9))
+			.whereToStay("리조트")
+			.addPlan(0, "체크인 후 짐 풀기")
+			.addPlan(0, "저녁 식사")
+			.getPlan();
+
+		TourPlan longBeachTrip = builder.title("롱비치")
+			.startDate(LocalDate.of(2021, 7, 30))
+			.getPlan();
+
+		TourDirector tourDirector = new TourDirector(new DefaultTourBuilder());
+		TourPlan tourPlan = tourDirector.cancunTrip();
+		TourPlan tourPlan2 = tourDirector.longBeachTrip();
 	}
 }
--- gof-design-pattern/src/main/java/com/seungjjun/designpattern/builder/TourDirector.java
+++ gof-design-pattern/src/main/java/com/seungjjun/designpattern/builder/TourDirector.java
@@ -1,2 +1,28 @@
-package com.seungjjun.designpattern.builder;public class TourDirector {
+package com.seungjjun.designpattern.builder;
+
+import java.time.LocalDate;
+
+public class TourDirector {
+
+	private TourPlanBuilder tourPlanBuilder;
+
+	public TourDirector(TourPlanBuilder tourPlanBuilder) {
+		this.tourPlanBuilder = tourPlanBuilder;
+	}
+
+	public TourPlan cancunTrip() {
+		return tourPlanBuilder.title("여행")
+			.nightsAndDays(2, 3)
+			.startDate(LocalDate.of(2020, 12, 9))
+			.whereToStay("리조트")
+			.addPlan(0, "체크인 후 짐 풀기")
+			.addPlan(0, "저녁 식사")
+			.getPlan();
+	}
+
+	public TourPlan longBeachTrip() {
+		return tourPlanBuilder.title("롱비치")
+			.startDate(LocalDate.of(2021, 7, 30))
+			.getPlan();
+	}
 }
