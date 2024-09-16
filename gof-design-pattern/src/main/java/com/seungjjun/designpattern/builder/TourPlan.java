cc4e0732dd5c7b79f7ccb9d68330a1448e1c0f77
seungjjun <il1uso@naver.com>
2024-09-16 14:15:09 +0900

    builder pattern

---
+++ gof-design-pattern/src/main/java/com/seungjjun/designpattern/builder/TourPlan.java
@@ -0,0 +1,44 @@
+package com.seungjjun.designpattern.builder;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class TourPlan {
+
+	private String title;
+
+	private int nights;
+
+	private int days;
+
+	private LocalDate startDate;
+
+	private String whereToStay;
+
+	private List<DetailPlan> plans;
+
+	public TourPlan() {
+	}
+
+	public TourPlan(String title, int nights, int days, LocalDate startDate, String whereToStay,
+					List<DetailPlan> plans) {
+		this.title = title;
+		this.nights = nights;
+		this.days = days;
+		this.startDate = startDate;
+		this.whereToStay = whereToStay;
+		this.plans = plans;
+	}
+
+	@Override
+	public String toString() {
+		return "TourPlan{" +
+			"title='" + title + '\'' +
+			", nights=" + nights +
+			", days=" + days +
+			", startDate=" + startDate +
+			", whereToStay='" + whereToStay + '\'' +
+			", plans=" + plans +
+			'}';
+	}
+}
---
+++ gof-design-pattern/src/main/java/com/seungjjun/designpattern/builder/TourPlanBuilder.java
@@ -0,0 +1,2 @@
+package com.seungjjun.designpattern.builder;public class TourPlanBuilder {
+}
